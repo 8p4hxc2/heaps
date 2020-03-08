30b444c9cf99ca35b8dbb24a04b47c017a4ed510
8p4hxc2 <8p4hxc2@gmail.com>
2020-03-08 22:15:06 +0100

    update sprite component

--- components/Sprite.hx
+++ components/Sprite.hx
@@ -2,10 +2,9 @@ package components;
 
 import core.Component;
 
-class Text extends Component {
+class Sprite extends Component {
 	public var texture:String;
-	public var instance:h2d.Sprite;
-  
+
 	public function new(_texture:String) {
 		texture = _texture;
 	}
